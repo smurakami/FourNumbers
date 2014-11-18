ed0717aceb9017e66fa84a79e9810c1f0f322c09
s_murakami <s.murakami@conol.jp>
2014-11-18 12:37:30 +0900

    Initial Commit

---
+++ FourNumbers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FourNumbers
+//
+//  Created by 村上晋太郎 on 2014/11/18.
+//  Copyright (c) 2014年 村上晋太郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
